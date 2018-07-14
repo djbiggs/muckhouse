86855d3ca9e546b66cd1bfd38f1c8d5e153a1218
Allard van Helbergen <allard@vanhelbergen.com>
2018-07-14 14:31:33 +1000

    merge commit

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,9 +1,11 @@
-##Preprocessing Images to minimize non target edges
-## Solving for 1 needs to be rewritten for set based.
+"""
+Preprocesses images to minimize non target edges.
+"""
 
 import cv2
 import numpy as np
 import math
+
 
 img = cv2.imread('test_img_6.jpg', 0)
 
@@ -26,7 +28,7 @@ img = cv2.medianBlur(img, ksize = 7)
 cv2.imshow('Dilation Median Blur', img)
 cv2.waitKey(0)
 
-###Gaussian Blur to disturb smaller edges rather than the larger. 
+###Gaussian Blur to disturb smaller edges rather than the larger.
 ##img = cv2.GaussianBlur(src = img, ksize = (3,3), sigmaX = 3)
 
 #cv2.imshow('GBlur', img)
@@ -47,7 +49,7 @@ img = cv2.Canny(img, threshold1 = 20000, threshold2 = 50000 ,apertureSize = 7)
 
 cv2.imshow('CannyEdge', img)
 cv2.waitKey(0)
-  
+
 kernel2 = np.ones((2,2), np.uint8)
 img = cv2.dilate(img, kernel2, iterations=2)
 
@@ -56,7 +58,7 @@ cv2.waitKey(0)
 
 ## Hough lines feature picking
 ##hlines =cv2.HoughLinesP(candm, rho = 1, theta = math.pi/180, threshold = 70, minLineLength = 100, maxLineGap = 10)
- 
+
 #Contour Search, method takes just the corner coordinates-- need to pass this,creates a numpy list of non redundant contour corner points
 img, contours, hierarchy = cv2.findContours(img, mode = cv2.RETR_LIST, method = cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img, contours, 5, (100,100,255), 2)
@@ -66,7 +68,7 @@ cv2.imshow('FinalContours', img)
 ## Display
 
 ##cv2.imshow('Hough Lines', hlines)
- 
+
 
 cv2.waitKey(0)
 
